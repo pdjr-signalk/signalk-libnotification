b626743380c18b45343da3ef565c70bebcff278f
Paul Reeve <preeve@pdjr.eu>
2020-05-30 10:43:25 +0200

    Refactor

--- Notification.js
+++ Notification.js
@@ -8,7 +8,7 @@ module.exports = class Notification {
         this.options = options;
     }
 
-    static issueNotification(key, message, state, method) {
+    static issue(key, message, state, method) {
         state = (state)?state:((this.options.state)?this.options.state:"normal");
         method = (method?((Array.isArray(method))?method:[method]):this.options.method;
         var delta = { "context": "vessels." + this.id, "updates": [ { "source": { "label": "self.notificationhandler" }, "values": [ { "path": key, "value": null } ] } ] };
@@ -16,7 +16,7 @@ module.exports = class Notification {
         this.handler(this.id, delta);
     }
 
-    static cancelNotification(key) {
+    static cancel(key) {
         var delta = { "context": "vessels." + this.id, "updates": [ { "source": { "label": "self.notificationhandler" }, "values": [ { "path": key, "value": null } ] } ] };
         this.handler(this.id, delta);
     }
