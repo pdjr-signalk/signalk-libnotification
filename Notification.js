3680403292801f1bb06636448a1038e4bbcbe2d0
Paul Reeve <preeve@pdjr.eu>
2020-05-30 10:16:15 +0200

    First upload

---
+++ Notification.js
@@ -0,0 +1,24 @@
+"use strict;"
+
+module.exports = class Notification {
+
+    constructor(handler, id, options={}) {
+        this.handler = handler;
+        this.id = id;
+        this.options = options;
+    }
+
+    static issueNotification(key, message, state, method) {
+        state = (state)?state:((this.options.state)?this.options.state:"normal");
+        method = (method?((Array.isArray(method))?method:[method]):this.options.method;
+        var delta = { "context": "vessels." + this.id, "updates": [ { "source": { "label": "self.notificationhandler" }, "values": [ { "path": key, "value": null } ] } ] };
+        delta.updates[0].values[0].value = { "state": state, "message": message, "method": method, "timestamp": (new Date()).toISOString() };
+        this.handler(this.id, delta);
+    }
+
+    static cancelNotification(key) {
+        var delta = { "context": "vessels." + this.id, "updates": [ { "source": { "label": "self.notificationhandler" }, "values": [ { "path": key, "value": null } ] } ] };
+        this.handler(this.id, delta);
+    }
+
+}
