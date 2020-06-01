fd2ca3981d017db5e416bc25d5f78ee6aecc19d7
Paul Reeve <preeve@pdjr.eu>
2020-06-01 12:19:43 +0200

    Revised interface

--- Notification.js
+++ Notification.js
@@ -2,27 +2,24 @@
 
 module.exports = class Notification {
 
-    constructor(handler, id, options={}) {
-        this.handler = handler;
+    constructor(app, id, options={}) {
+        this.app = app;
         this.id = id;
         this.options = options;
     }
 
     issue(key, message, options={}) {
-        var state = (this.options.state)?this.options.state:"normal";
-        var method = (this.options.method)?this.options.method:[];
+        var state = (options.state)?options.state:((this.options.state)?this.options.state:"normal");
+        var method = (options.method)?options.method:((this.options.method)?this.options.method:[]);
 
-        state = (options.state)?options.state:state;
-        method = (options.method)?options.method:method;
-        
-        var delta = { "context": "vessels." + this.id, "updates": [ { "source": { "label": "self.notificationhandler" }, "values": [ { "path": key, "value": null } ] } ] };
+        var delta = { "context": "vessels." + this.app.selfId, "updates": [ { "source": { "label": "self.notificationhandler" }, "values": [ { "path": key, "value": null } ] } ] };
         delta.updates[0].values[0].value = { "state": state, "message": message, "method": method, "timestamp": (new Date()).toISOString() };
-        this.handler(this.id, delta);
+        this.app.handleMessage(this.id, delta);
     }
 
     cancel(key) {
-        var delta = { "context": "vessels." + this.id, "updates": [ { "source": { "label": "self.notificationhandler" }, "values": [ { "path": key, "value": null } ] } ] };
-        this.handler(this.id, delta);
+        var delta = { "context": "vessels." + this.app.selfId, "updates": [ { "source": { "label": "self.notificationhandler" }, "values": [ { "path": key, "value": null } ] } ] };
+        this.app.handleMessage(this.id, delta);
     }
 
 }
